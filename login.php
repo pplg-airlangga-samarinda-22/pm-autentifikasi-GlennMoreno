af1c4606ff141709397e561d215ed418612e350c
GlennMoreno <lolglenn26@gmail.com>
2025-01-15 13:24:52 +0800

    udah pak

--- login.php
+++ login.php
@@ -1,47 +1,49 @@
 <?php
 require "koneksi.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nik = $_POST['nik'];
     $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    //fungsi execute_query hanya bisa digunakan pada php8.2
-    $query = "SELECT * FROM masyarakat WHERE nik = '$nik' AND username = '$username' AND password = '$password'";
-    $row = $koneksi->execute_query($sql, [$nik, $username, $password]);
-
-    if (mysqli_num_rows($row) == 1) {
+    $password = md5($_POST['password']);
+    
+    $sql = "SELECT*FROM masyarakat WHERE nik = ? AND username = ? AND password = ?";
+    $row = $koneksi -> execute_query($sql, [$nik, $username, $password]);
+    
+    if (mysqli_num_rows($row) ==  1) {
         session_start();
-        $_SESSION['nik'] = $nil;
+        $_SESSION['nik'] = $nik;
         header("location:index.php");
     } else {
-        echo "<script>alert ('gagal login!')</script>";
+        echo"<script>alert('Gagal Login!')</script>";
     }
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Login</title>
+    <title>login</title>
 </head>
+
 <body>
-    <form action="" method="post" class="form-login">
-        <p>Silahkan Login</p>
-        <div class="form-item">
-            <label for="nik">NIK</label>
-            <input type="text" name="nik" id="nik" required>
-        </div>
-        <div class="form-item">
-            <label for="nik">Username</label>
-            <input type="text" name="username" id="username" required>
-        </div>
-        <div class="form-item">
-            <label for="nik">Passwword</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <button type="submit">Login</button>
-        <a href="register.php">Register</a>
+    <form action=""method="post" class="form-login">
+    <p>Silahkan Login</p>
+    <div class="form-item">
+        <label for="nik">nik</label>
+        <input type="text"name="nik" id="nik" required>
+    </div>
+    <div class="form-item">
+        <label for="username">username</label>
+        <input type="text"name="username" id="username" required>
+    </div>
+    <div class="form-item">
+        <label for="password">password</label>
+        <input type="password"name="password" id="password" required>
+    </div>
+    <button type="submit">Login</button>
+    <a href="register.php">Register</a>
     </form>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,27 +1,28 @@
 <?php
 require "koneksi.php";
 
-if ($_server['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $nik = $_POST['nik'];
 
-    //cek dulu masse nik nya
     $sql = "SELECT * FROM masyarakat WHERE nik=?";
     $cek = $koneksi->execute_query($sql, [$nik]);
 
-    if(mysqli_num_rows($cek) == 1) {
-        echo "<script>alert('nik sudah digunakan!')</script>";
+    if (mysqli_num_rows($cek) == 1) {
+        echo "<script>alert('NIK sudah digunakan!')</script>";
     } else {
         $nama = $_POST['nama'];
         $telepon = $_POST['telepon'];
         $username = $_POST['username'];
-        $password = $_POST['password'];
-        $sql = "INSERT INTO masyarakat SET nik=?, nama=?, telepon=?, username=?, password=?";
+        $password = md5($_POST['password']);
+        $sql = "INSERT INTO masyarakat SET nik=?, nama=?, telp=?, username=?, password=?";
         $koneksi->execute_query($sql, [$nik, $nama, $telepon, $username, $password]);
-        echo "<script>alert('berhasil register!')</script>";
+        echo "<script>alert('Pendaftaran berhasil!')</script>";
         header("location:login.php");
     }
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,15 +34,15 @@ if ($_server['REQUEST_METHOD'] == "POST") {
     <form action="" method="post">
         <div class="form-item">
             <label for="nik">NIK</label>
-            <input type="number" name="nik" id="nik">
+            <input type="text" name="nik" id="nik"  >
         </div>
         <div class="form-item">
-            <label for="nama">Nama</label>
+            <label for="nama">Nama Lengkap</label>
             <input type="text" name="nama" id="nama">
         </div>
         <div class="form-item">
             <label for="telepon">Telepon</label>
-            <input type="number" name="telepon" id="telepon">
+            <input type="tel" name="telepon" id="telepon">
         </div>
         <div class="form-item">
             <label for="username">Username</label>
